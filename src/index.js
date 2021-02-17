c25ebaaa8ec27e0cd201daa87b5346099363e9dd
Alexey <69269929+NewAlexey@users.noreply.github.com>
2021-02-17 22:52:38 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) return 0;
+    else {
+        return array.reduce((acc, item) => {
+            if (acc > item) {
+                acc = item;
+                return acc;
+            }
+            return acc;
+        }, array[0]);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) return 0;
+    else {
+        return array.reduce((acc, item) => {
+            if (acc < item) {
+                acc = item;
+                return acc;
+            }
+            return acc;
+        }, array[0]);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) return 0;
+    else {
+        let avg = 0;
+        array.forEach((element) => {
+            avg += element;
+        });
+        return avg / array.length;
+    }
+};
